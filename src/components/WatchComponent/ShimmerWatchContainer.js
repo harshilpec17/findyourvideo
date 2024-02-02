d3f1cbe398593677e8dbafb93bbb932a81456011
harshilpec17 <harshilsuthar1995@gmail.com>
2024-02-02 14:31:32 -0500

    add: shimmerContainer

---
+++ src/components/WatchComponent/ShimmerWatchContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ShimmerWatchContainer = () => {
+  return (
+    <div className="h-screen w-screen bg-red-700">
+      <h1 className="text-9xl bg-red-400 text-center h-screen">
+        Loading.........
+      </h1>
+    </div>
+  );
+};
+
+export default ShimmerWatchContainer;
